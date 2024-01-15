8f830b2a950ea09c28e02d95c0a9bd706a1c1f4c
Alexey-zaliznuak <zaliznuak20@gmail.com>
2024-01-15 23:02:52 +0300

    first commit

--- src/domain/employer/employer.prisma-extension.ts
+++ src/domain/employer/employer.prisma-extension.ts
@@ -1,4 +1,5 @@
 import { Prisma } from '@prisma/client'
+import { HttpError } from '../../infrastructure/error/httpError';
 
 
 export const employerPrismaExtension = Prisma.defineExtension({
@@ -15,7 +16,34 @@ export const employerPrismaExtension = Prisma.defineExtension({
   model: {
     employer: {
       async archive(id: string) {
-        return {"test": "testing"}
+        const context = Prisma.getExtensionContext(this)
+const result = await (context as any).findFirst({ where })
+
+        const employer = await prisma.employer.findUnique(
+          {
+            where: {id},
+            include: {
+              lega: true,
+              password: true,
+              resume: true,
+              meetings: true,
+              assignedVacancies: true,
+              offers: true,
+            },
+          })
+
+        if (!employer) {
+          throw new HttpError(404, "Employer not found")
+        }
+
+        await prisma.softArchive.create({
+          data: {
+            modelName: "Applicant",
+            originalId: employer.id,
+            payload: JSON.stringify(employer),
+          },
+        })
+        prisma.applicant.delete({where: {id}})
       },
     },
   },
