c9e44640e0174125f93cf1b196cd13bcc5144d4e
Artemiy Prikupets <artemprikupets@gmail.com>
2024-01-17 17:40:28 +0400

    chore

--- src/domain/applicant/applicant.prisma-extension.ts
+++ src/domain/applicant/applicant.prisma-extension.ts
@@ -1,6 +1,6 @@
 import { Prisma } from "@prisma/client";
-import { prisma } from '../../infrastructure/database/prismaClient';
-import { HttpError } from '../../infrastructure/error/httpError';
+import { prisma } from "../../infrastructure/database/prismaClient";
+import { HttpError } from "../../infrastructure/error/httpError";
 
 
 export const applicantPrismaExtension = Prisma.defineExtension({
@@ -42,24 +42,24 @@ export const applicantPrismaExtension = Prisma.defineExtension({
         })
 
         await prisma.$transaction([
-          prisma.hhToken.delete( { where: { applicantId: id } } ),
-          prisma.password.deleteMany( { where: { applicant: { id: applicant.id}} } ),
+          prisma.hhToken.delete({ where: { applicantId: id } }),
+          prisma.password.deleteMany({ where: { applicant: { id: applicant.id } } }),
 
-          prisma.resumeCertificate.deleteMany( { where: { resumeId: applicant.resume?.id } } ),
-          prisma.resumeContact.deleteMany( { where: { resumeId: applicant.resume?.id } } ),
-          prisma.resumeEducation.deleteMany( { where: { resumeId: applicant.resume?.id } } ),
-          prisma.resumeExperience.deleteMany( { where: { resumeId: applicant.resume?.id } } ),
-          prisma.resumeLanguage.deleteMany( { where: { resumeId: applicant.resume?.id } } ),
-          prisma.resume.delete( { where: { applicantId: id } } ),
+          prisma.resumeCertificate.deleteMany({ where: { resumeId: applicant.resume?.id } }),
+          prisma.resumeContact.deleteMany({ where: { resumeId: applicant.resume?.id } }),
+          prisma.resumeEducation.deleteMany({ where: { resumeId: applicant.resume?.id } }),
+          prisma.resumeExperience.deleteMany({ where: { resumeId: applicant.resume?.id } }),
+          prisma.resumeLanguage.deleteMany({ where: { resumeId: applicant.resume?.id } }),
+          prisma.resume.delete({ where: { applicantId: id } }),
 
-          prisma.meetingFeedback.deleteMany( {  where: {  meeting: { applicantId: id } } } ),
-          prisma.meetingScriptAnswer.deleteMany( { where: { protocol: {meeting: {applicantId: applicant.id } } } } ),
-          prisma.meetingScriptProtocol.deleteMany( { where: { meeting: {applicantId: applicant.id} } } ),
-          prisma.meeting.deleteMany( { where: { applicantId: id} } ),
+          prisma.meetingFeedback.deleteMany({ where: { meeting: { applicantId: id } } }),
+          prisma.meetingScriptAnswer.deleteMany({ where: { protocol: { meeting: { applicantId: applicant.id } } } }),
+          prisma.meetingScriptProtocol.deleteMany({ where: { meeting: { applicantId: applicant.id } } }),
+          prisma.meeting.deleteMany({ where: { applicantId: id } }),
 
-          prisma.offer.deleteMany( { where: { candidateId: id} } ),
+          prisma.offer.deleteMany({ where: { candidateId: id } }),
 
-          prisma.applicant.delete( { where: { id } } ),
+          prisma.applicant.delete({ where: { id } }),
         ])
       },
     },
