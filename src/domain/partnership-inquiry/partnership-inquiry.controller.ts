8eb458b3799d62c89b4d00df616b1dc8c1c4476b
Alexey-zaliznuak <zaliznuak20@gmail.com>
2024-01-20 20:59:42 +0300

    fix request annotations

--- src/domain/partnership-inquiry/partnership-inquiry.controller.ts
+++ src/domain/partnership-inquiry/partnership-inquiry.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Get, Path, Post, Put, Query, Response, Route, Security, Tags } from "tsoa";
-import { BasicPartnershipInquiry, CreatePartnershipInquiryRequest } from "./partnership-inquiry.dto";
+import {
+  BasicPartnershipInquiry,
+  CreatePartnershipInquiryRequest,
+  PutPartnershipInquiryStatus,
+} from "./partnership-inquiry.dto";
 import { prisma } from "../../infrastructure/database/prismaClient";
 import { UserRole } from "../auth/auth.dto";
 import { PageResponse } from "../../infrastructure/controller/pagination/page.response";
 import { HttpError, HttpErrorBody } from "../../infrastructure/error/httpError";
-import { PartnershipInquiryStatus } from "@prisma/client";
 import { injectable } from "tsyringe";
 import { PartnershipInquiryService } from "./partnership-inquiry.service";
 import { PageNumber, PageSizeNumber } from "../../infrastructure/controller/pagination/page.dto";
@@ -55,7 +58,7 @@ export class PartnershipInquiryController extends Controller {
   @Response<HttpErrorBody & {"error": "PartnershipInquiry not found"}>(404)
   public async putStatus(
     @Path() id: string,
-    @Body() status: PartnershipInquiryStatus,
+    @Body() body: PutPartnershipInquiryStatus,
   ): Promise<BasicPartnershipInquiry> {
     const partnershipInquiry = await prisma.partnershipInquiry.findUnique({
       where: { id },
@@ -65,7 +68,7 @@ export class PartnershipInquiryController extends Controller {
 
     return prisma.partnershipInquiry.update({
       where: { id },
-      data: { status },
+      data: { status: body.status },
     });
   }
 
