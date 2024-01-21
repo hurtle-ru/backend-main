f03c79e7a6f57423e4161984042a5d2df2e7d296
Artemiy Prikupets <artemprikupets@gmail.com>
2024-01-22 01:56:58 +0400

    chore/refactor

--- src/infrastructure/controller/date/date.dto.ts
+++ src/infrastructure/controller/date/date.dto.ts
@@ -4,9 +4,3 @@
  * @example "2024-01-20"
  */
 export type DateWithoutTime = Date;
-
-/**
- * @pattern ^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/(20\d{2})$
- * @example "31/12/2023"
- */
-export type UtcDate = string
