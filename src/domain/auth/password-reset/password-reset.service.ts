7d821d9f6844e06821bbcef27d75cfd65411f736
Alexey-zaliznuak <zaliznuak20@gmail.com>
2024-02-05 21:09:20 +0300

    update code format, add short verify-email link

--- src/domain/auth/password-reset/password-reset.service.ts
+++ src/domain/auth/password-reset/password-reset.service.ts
@@ -2,6 +2,7 @@ import { appConfig } from "../../../infrastructure/app.config";
 import { injectable, singleton } from "tsyringe";
 import { MailService } from "../../../external/mail/mail.service";
 import otpGenerator from "otp-generator";
+import path from "path";
 
 
 @injectable()
@@ -10,13 +11,13 @@ export class PasswordResetService {
   constructor(private readonly mailService: MailService) {}
 
   generateCode(): string {
-    return otpGenerator.generate(6, { upperCaseAlphabets: false, specialChars: false });
+    return otpGenerator.generate(6, {lowerCaseAlphabets: false, upperCaseAlphabets: false, specialChars: false });
   }
 
   async sendEmail(email: string, code: string) {
     await this.mailService.sendEmail(email, "Сброс пароля", {
       name: "reset-password",
-      context: { code },
+      context: { code, link: appConfig.DOMAIN + `/auth/verify-email/${code}`},
     });
   }
 }
