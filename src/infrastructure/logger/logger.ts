2cb9f07ecf9eec025aaa62575d245066994e5664
Artemiy Prikupets <artemprikupets@gmail.com>
2024-03-07 03:02:36 +0400

    feat(pine-logging): WIP on logger

--- src/infrastructure/logger/logger.ts
+++ src/infrastructure/logger/logger.ts
@@ -1,13 +1,10 @@
 import pino from "pino";
 import pinoHttp from "pino-http";
 import fs from "fs";
+import path from "path";
 
 
-const logsDir = "./logs";
-if (!fs.existsSync(logsDir)) {
-  fs.mkdirSync(logsDir, { recursive: true });
-}
-
+const logFilePath = initLogCounter();
 const logger = pino({
   level: "trace",
   base: null,
@@ -16,16 +13,16 @@ const logger = pino({
     targets: [
       {
         level: "trace",
-        target: "pino/file",
-        options: { destination: `${logsDir}/.log` },
-      },
-      {
-        level: "trace",
         target: "pino-pretty",
         options: {
           translateTime: "dd-mm-yyyy HH:MM:ss",
           ignore: "pid,hostname",
         },
+      },
+      {
+        level: "trace",
+        target: "pino/file",
+        options: { destination: logFilePath },
       },
     ],
   },
@@ -39,8 +36,19 @@ const httpLogger = pinoHttp({
       method: req.method,
       query: req.query,
       params: req.params,
-      headers: req.headers,
-      ip: req.headers["X-Forwarded-For"] || req.remoteAddress,
+      headers: {
+        "content-type": req.headers["content-type"],
+        "content-length": req.headers["content-length"],
+        "authorization": req.headers["authorization"],
+      },
+      ip: req.headers["x-forwarded-for"] || req.remoteAddress,
+    }),
+    res: (res) => ({
+      statusCode: res.statusCode,
+      headers: {
+        "content-type": res.headers["content-type"],
+        "content-length": res.headers["content-length"],
+      },
     }),
   },
   customLogLevel: function (req, res, err) {
@@ -54,5 +62,33 @@ const httpLogger = pinoHttp({
   },
   quietReqLogger: true,
 });
+
+function initLogCounter(): string {
+  const logsDir = "data/logs";
+  const logCounterFile = path.join(logsDir, "logCounter.txt");
+
+  if (!fs.existsSync(logsDir)) {
+    fs.mkdirSync(logsDir, { recursive: true });
+  }
+
+  const nextLogNumber = getNextLogFileNumber(logCounterFile);
+  return `${logsDir}/${nextLogNumber}.log`;
+}
+
+function getNextLogFileNumber(logCounterFile: string) {
+  let lastLogNumber = 0;
+
+  if (fs.existsSync(logCounterFile)) {
+    const counterContents = fs.readFileSync(logCounterFile, { encoding: "utf8" });
+    lastLogNumber = parseInt(counterContents, 10);
+  }
+
+  const nextLogNumber = lastLogNumber + 1;
+
+  fs.writeFileSync(logCounterFile, nextLogNumber.toString(), { encoding: "utf8" });
+
+  return nextLogNumber;
+}
+
 
 export { logger, httpLogger };
