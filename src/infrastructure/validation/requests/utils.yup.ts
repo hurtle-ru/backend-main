4099737b0f4064c438911f034adbf910377c2b9d
Alexey-zaliznuak <zaliznuak20@gmail.com>
2024-03-27 22:33:43 +0300

    small fixes

--- src/infrastructure/validation/requests/utils.yup.ts
+++ src/infrastructure/validation/requests/utils.yup.ts
@@ -4,12 +4,12 @@ import * as yup from "yup";
 export function validateSyncByAtLeastOneSchema(schemas: yup.Schema[], body: any): void {
   let firstError: any = null;
 
-  schemas.forEach((schema) => {
+  for (let schema of schemas) {
     try {
       schema.validateSync(body);
       return;
-    } catch (error) { firstError = firstError ?? error }
-  });
+    } catch (error) { firstError = firstError ?? error; console.log(error) }
+  };
 
   throw firstError;
 }
