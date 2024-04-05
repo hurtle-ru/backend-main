29119f7d569506717f3a910e3abde4241cebe34c
Alexey <zaliznuak20@gmail.com>
2024-04-05 18:26:24 +0300

    fix typo

--- src/external/admin-panel/admin-panel.service.ts
+++ src/external/admin-panel/admin-panel.service.ts
@@ -5,6 +5,6 @@ import { singleton } from "tsyringe";
 export class AdminPanelService {
     private AdminPanelUrl = "https://service.hurtle.ru/"
 
-    public getLinkOnMeeting = (meetingId: string): string => this.AdminPanelUrl + "meetings/"   + meetingId
+    public getLinkOnMeeting = (meetingId: string): string => this.AdminPanelUrl + "meetings/"  + meetingId
     public getLinkOnVacancy = (vacancyId: string): string => this.AdminPanelUrl + "vacancies/" + vacancyId
 }
