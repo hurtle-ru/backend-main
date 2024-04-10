084531704e6ac798c8228676f09f8202214548ed
Alexey-zaliznuak <zaliznuak20@gmail.com>
2024-04-10 16:39:58 +0300

    validation time

--- src/domain/meeting/script/template/template.dto.ts
+++ src/domain/meeting/script/template/template.dto.ts
@@ -1,6 +1,9 @@
+import * as yup from 'yup'
+
 import { MeetingScriptTemplate } from "@prisma/client";
 import { BasicMeetingScriptQuestion } from "../question/question.dto";
 import { BasicMeetingScriptProtocol } from "../protocol/protocol.dto";
+
 
 export type BasicMeetingScriptTemplate = Omit<
   MeetingScriptTemplate,
@@ -8,13 +11,32 @@ export type BasicMeetingScriptTemplate = Omit<
   | "questions"
 >;
 
+export const BasicMeetingScriptTemplateSchema: yup.ObjectSchema<BasicMeetingScriptTemplate> = yup.object({
+  id: yup.string().defined(),
+  createdAt: yup.date().defined(),
+  updatedAt: yup.date().defined(),
+  title: yup.string().defined().max(100),
+  description: yup.string().defined().max(300),
+  answerOptions: yup.array(yup.string().defined().max(300)).defined(),
+})
+
 export type CreateMeetingScriptTemplateRequest = Pick<
   MeetingScriptTemplate,
   | "title"
   | "description"
 >;
+export const CreateMeetingScriptTemplateRequestSchema: yup.ObjectSchema<CreateMeetingScriptTemplateRequest> = BasicMeetingScriptTemplateSchema.pick([
+  "title",
+  "description"
+])
 
-export type PutMeetingScriptTemplateRequest = CreateMeetingScriptTemplateRequest;
+
+export type PatchMeetingScriptTemplateRequest = Partial<CreateMeetingScriptTemplateRequest>;
+export const PatchMeetingScriptTemplateRequestSchema: yup.ObjectSchema<PatchMeetingScriptTemplateRequest> = BasicMeetingScriptTemplateSchema.pick([
+  "title",
+  "description"
+]).partial()
+
 
 export type GetMeetingScriptTemplateResponse = BasicMeetingScriptTemplate & {
   protocols?: BasicMeetingScriptProtocol[];
