3a256f40e6c58e31a3b7a657679ff091849395cc
Artemiy Prikupets <artemprikupets@gmail.com>
2024-04-25 18:26:10 +0400

    feat(86byf7ea6): WIP on guest response API + OCR;

--- src/domain/vacancy/guest-response/guest-response.dto.ts
+++ src/domain/vacancy/guest-response/guest-response.dto.ts
@@ -35,7 +35,7 @@ export type CreateGuestVacancyResponseRequestResume = Pick<
     | "name"
     | "description"
     | "year"
-    >[];
+  >[];
   contacts: Pick<
     BasicResumeContact,
     | "name"
